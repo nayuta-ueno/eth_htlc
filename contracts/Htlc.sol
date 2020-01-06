d80bee38a44077de9a87f8f34a99dfbc0465711d
ueno <ueno@nayuta.co>
2020-01-06 20:29:36 +0900

    tests

--- contracts/Htlc.sol
+++ contracts/Htlc.sol
@@ -1,39 +1,68 @@
 pragma solidity ^0.5.0;
 
 contract Htlc {
-    bytes32 paymentHash;
-    address payable payer;
-    uint amount;
-    bool pooled;
+    struct HtlcData {
+        bytes32 paymentHash;
+        address payable payer;
+        uint amount;
+        uint height;
+        uint delay;
+        bool pooled;
+    }
+    mapping(address => HtlcData) htlcData;
 
-    function poolPayment(bytes32 _paymentHash, address payable _payer, uint _amount) public {
-        if (!pooled) {
-            require(payer.balance > _amount, "need more balance");
-            paymentHash = _paymentHash;
-            payer = _payer;
-            amount = _amount;
-            pooled = true;
+    function poolPayment(bytes32 _paymentHash, address payable _payer, uint _amount, uint _delay) public {
+        if (!htlcData[msg.sender].pooled) {
+            require(msg.sender.balance > _amount, "need more balance");
+            htlcData[msg.sender].paymentHash = _paymentHash;
+            htlcData[msg.sender].payer = _payer;
+            htlcData[msg.sender].amount = _amount;
+            htlcData[msg.sender].height = block.number; //miningされたときのblockNumberになるようだ
+            htlcData[msg.sender].delay = _delay;
+            htlcData[msg.sender].pooled = true;
         }
     }
 
+    //set系は戻り値がどうやってもtxになる
     function payment(bytes32 _preImage, address payable _payer) public payable {
-        if (pooled) {
-            require(payer == _payer, "bad payer");
-            require(payer.balance > amount, "bad balance.");
+        if (htlcData[msg.sender].pooled) {
+            require(htlcData[msg.sender].payer == _payer, "bad payer");
+            require(msg.sender.balance > htlcData[msg.sender].amount, "bad balance.");
             bytes32 _hash = sha256(abi.encodePacked(_preImage));
-            if (_hash == paymentHash) {
-                payer.transfer(amount);
-                pooled = false;
+            if (_hash == htlcData[msg.sender].paymentHash) {
+                htlcData[msg.sender].payer.transfer(htlcData[msg.sender].amount);
+                htlcData[msg.sender].pooled = false;
             }
         }
     }
 
+    function withdraw() public {
+        if ( htlcData[msg.sender].pooled &&
+             (block.number - htlcData[msg.sender].height > htlcData[msg.sender].delay)) {
+            htlcData[msg.sender].pooled = false;
+        } else {
+            assert(false);
+        }
+    }
+
     function checkPreimage(bytes32 _preImage) public view returns(bool) {
-        return sha256(abi.encodePacked(_preImage)) == paymentHash;
+        if (htlcData[msg.sender].pooled) {
+            return sha256(abi.encodePacked(_preImage)) == htlcData[msg.sender].paymentHash;
+        } else {
+            return false;
+        }
     }
 
     function isPooled() public view returns(bool) {
-        return pooled;
+        return htlcData[msg.sender].pooled;
+    }
+
+    function getMinimumTimeout() public view returns(uint) {
+        if (htlcData[msg.sender].pooled) {
+            return htlcData[msg.sender].height + htlcData[msg.sender].delay + 1;
+        } else {
+            return 0;
+        }
     }
 
     function () external payable {}
--- test/TestHtlc.js
+++ test/TestHtlc.js
@@ -6,11 +6,17 @@ contract("Htlc", async accounts => {
 
     assert.isFalse(await htlc.isPooled(), "deployed");
 
+    var height = await web3.eth.getBlockNumber();
+
     //sha256(001122334455667788990011223344556677889900112233445566778899aabb)
     //-->0f21c51a169a3a60dcd7a5e9ca0aead03027e3c3d36646d992145becfcf1d8d8
-    await htlc.poolPayment("0x0f21c51a169a3a60dcd7a5e9ca0aead03027e3c3d36646d992145becfcf1d8d8", accounts[0], 10000);
+    await htlc.poolPayment(
+        "0x0f21c51a169a3a60dcd7a5e9ca0aead03027e3c3d36646d992145becfcf1d8d8",
+        accounts[0], 10000, 2);
 
     assert.isTrue(await htlc.isPooled(), "poolPayment");
+    assert.equal(await web3.eth.getBlockNumber(), height + 1, "1block");
+    assert.equal(await htlc.getMinimumTimeout(), height + 1 + 2 + 1, "timeout");
   });
 
   it("pay", async () => {
@@ -23,15 +29,13 @@ contract("Htlc", async accounts => {
     // var bal = await web3.eth.getBalance(accounts[0]);
     // console.log("bal=" + bal);
 
-    var pooled = await htlc.isPooled();
-    assert.isTrue(pooled, "poolPayment");
-    // console.log("pooled=" + pooled);
+    assert.isTrue(await htlc.isPooled(), "poolPayment");
 
     var preimage = "0x001122334455667788990011223344556677889900112233445566778899aabb";
 
     var ret = await htlc.checkPreimage(preimage);
-    // console.log("ret=" + ret);
     assert.isTrue(ret, "checkPreimage");
+
     try {
         await htlc.payment(preimage, accounts[0]);
     } catch (err) {
@@ -41,14 +45,15 @@ contract("Htlc", async accounts => {
     // bal = await web3.eth.getBalance(accounts[0]);
     // console.log("bal2=" + bal);
 
-    pooled = await htlc.isPooled();
-    assert.isFalse(pooled, "payment");
-    // console.log("pooled=" + pooled);
+    assert.isFalse(await htlc.isPooled(), "payment");
 
-    await htlc.poolPayment("0x0f21c51a169a3a60dcd7a5e9ca0aead03027e3c3d36646d992145becfcf1d8d8", accounts[0], 10000);
-    pooled = await htlc.isPooled();
-    assert.isTrue(pooled, "poolPayment");
-    // console.log("pooled=" + pooled);
+    var height = await web3.eth.getBlockNumber();
+    await htlc.poolPayment(
+        "0x0f21c51a169a3a60dcd7a5e9ca0aead03027e3c3d36646d992145becfcf1d8d8",
+        accounts[0], 10000, 2);
+    assert.isTrue(await htlc.isPooled(), "poolPayment");
+    assert.equal(await web3.eth.getBlockNumber(), height + 1, "1block");
+    assert.equal(await htlc.getMinimumTimeout(), height + 1 + 2 + 1, "timeout");
 
     preimage = "0x000122334455667788990011223344556677889900112233445566778899aabb";
     ret = await htlc.checkPreimage(preimage);
@@ -64,9 +69,26 @@ contract("Htlc", async accounts => {
     // bal = await web3.eth.getBalance(accounts[0]);
     // console.log("bal2=" + bal);
 
-    pooled = await htlc.isPooled();
-    assert.isTrue(pooled, "failPayment");
-    // console.log("pooled=" + pooled);
-  });
+    assert.isTrue(await htlc.isPooled(), "failPayment");
+    assert.equal(await web3.eth.getBlockNumber(), height + 2, "2block");
+
+    try {
+        await htlc.withdraw();
+        assert.isTrue(false);
+    } catch (err) {
+        //fail withdrawn
+    }
+
+    //await web3.eth.sendTransaction({from:accounts[0], to:accounts[0], value:1});
+    assert.equal(await web3.eth.getBlockNumber(), height + 3, "3block");
+    try {
+        await htlc.withdraw();
+    } catch (err) {
+        //fail withdrawn
+        assert.isTrue(false);
+    }
+
+    assert.isFalse(await htlc.isPooled(), "withdraw");
+});
 
 });
